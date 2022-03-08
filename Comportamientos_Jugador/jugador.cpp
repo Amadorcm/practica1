b862267d4cfcfabbf8dc7f77019cf005ecdbb202
Amadorcm <Amadorcm@corre.ugr.es>
2022-03-08 11:25:43 +0100

    Tutorial 1 con fallos de pintar e la matriz

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -30,8 +30,53 @@ Action ComportamientoJugador::think(Sensores sensores){
 	cout << "Vida: " << sensores.vida << endl;
 	cout << endl;
 
+	//Codigo nuuevo
+	switch (ultimaAccion){
+		case actFORWARD:
+			switch (brujula){
+				case 0: // Norte
+					fil--; break;
+				case 1: // Este
+					col++; break;
+				case 2: // Sur
+					fil++; break;
+				case 3: // Oeste
+					col--; break;
+			}
+			break;
+		case actTURN_L:
+			brujula = (brujula+3)%4;
+			girar_derecha=(rand()%2 ==0);
+			break;
+		case actTURN_R:
+			brujula = (brujula+1)%4;
+			girar_derecha=(rand()%2 ==0);
+			break;
+	}
+	//Regla que captura los valores de fila y columna en estas casillas
+	if (sensores.terreno[0]=='G' && !bien_situado){
+		fil = sensores.posF;
+		col= sensores.posC;
+		bien_situado = true;
+	}
+
+	if (bien_situado){
+		mapaResultado[fil][col]=sensores.terreno[0];
+	}
+	// determinamos el siguiente paso: 
+
+	if ((sensores.terreno[2]=='T' or sensores.terreno[2]=='S' or sensores.terreno[2]=='G') and (sensores.superficie[2]=='_')){
+		accion = actFORWARD;
+	}
+	else if (!girar_derecha){
+		accion = actTURN_L;
+	}
+	else{
+		accion = actTURN_R;	
+	}
 
 	// Determinar el efecto de la ultima accion enviada
+	ultimaAccion=accion;
 	return accion;
 }
 
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -12,13 +12,16 @@ class ComportamientoJugador : public Comportamiento{
       // Dar el valor inicial a las variables de estado
     }
 
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
+    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){fil=99,col=99,brujula=0,ultimaAccion=actIDLE,girar_derecha=false,bien_situado=false;}
     ~ComportamientoJugador(){}
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
 
   private:
+    int fil, col, brujula; 
+    Action ultimaAccion;
+    bool girar_derecha, bien_situado;
   
   // Declarar aquí las variables de estado
 
